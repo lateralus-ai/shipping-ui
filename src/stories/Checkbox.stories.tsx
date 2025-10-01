316ae861afc91a32ad6e5c63125d81a6504eedcb
Smyrn96 <emmanouilsmyrnakis@gmail.com>
2025-10-01 11:53:09 +0300

    fix: Center the check icon in checkbox

---
+++ src/stories/Checkbox.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { CheckboxItem } from "./Checkbox"
+
+const meta = {
+  title: "Design System/CheckboxItem",
+  component: CheckboxItem,
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof CheckboxItem>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
---
+++ src/stories/Checkbox.tsx
@@ -0,0 +1,56 @@
+import { Checkbox } from "@material-tailwind/react"
+import { Icon } from "@iconify/react"
+import { useState } from "react"
+
+export const CheckboxItem = () => {
+  const [isChecked, setIsChecked] = useState(false)
+
+  return (
+    <div className="p-8 grid gap-4">
+      <div className="flex flex-col gap-2">
+        <div className="font-medium text-lg">Default</div>
+        <Checkbox
+          checked={isChecked}
+          icon={<Icon icon="lucide:check" className="w-3.5 h-3.5" />}
+          onChange={() => {
+            setIsChecked(!isChecked)
+          }}
+          containerProps={{
+            className: "pl-1 pr-2 pb-0 pt-1",
+          }}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <div className="font-medium text-lg">With label</div>
+        <Checkbox
+          label="Remember Me"
+          checked={isChecked}
+          icon={<Icon icon="lucide:check" className="w-3.5 h-3.5" />}
+          onChange={() => {
+            setIsChecked(!isChecked)
+          }}
+          containerProps={{
+            className: "pl-1 pr-2 pb-0 pt-1",
+          }}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <div className="font-medium text-lg">Disabled</div>
+        <Checkbox
+          label="Remember Me"
+          checked={isChecked}
+          disabled={true}
+          icon={<Icon icon="lucide:check" className="w-3.5 h-3.5" />}
+          onChange={() => {
+            setIsChecked(!isChecked)
+          }}
+          containerProps={{
+            className: "pl-1 pr-2 pb-0 pt-1",
+          }}
+        />
+      </div>
+    </div>
+  )
+}
