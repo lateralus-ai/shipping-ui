23f05477339bd1cf51ff4ae249d213df17c94a8d
Marios Antonoudiou <marios@lateralus.ai>
2025-10-01 10:24:16 +0300

    Update story

--- src/stories/InputPrompt.tsx
+++ src/stories/InputPrompt.tsx
@@ -32,6 +32,19 @@ export const InputPrompts = () => {
       <InputPrompt.Container right={rightButtons} />
       <InputPrompt.Container right={rightButtons} bottom={bottomButtons} />
       <InputPrompt.Container right={rightButtons} top={topContent} />
+
+      <div className="border rounded-lg">
+        <div className="border-b p-4">Lorem Ipsum dorcet sit amet</div>
+        <div className="p-4 border-b">
+          <div className="h-[650px] text-gray-400 font-semibold bg-gray-100 rounded-2xl items-center justify-center flex">
+            Content placeholder
+          </div>
+        </div>
+        <InputPrompt.Container
+          right={rightButtons}
+          className="border-none shadow-none"
+        />
+      </div>
     </div>
   );
 };
